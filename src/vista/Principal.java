cdf6cfb81837e8d7a19a910b5db2f84eba8a6229
Iván Sánchez <isanchezg1@est.ups.edu.ec>
2021-01-20 16:24:40 -0500

    Se creo la vista factura.

--- src/vista/Principal.java
+++ src/vista/Principal.java
@@ -14,6 +14,7 @@ import java.awt.Dimension;
 public class Principal extends javax.swing.JFrame {
 
     private VistaProducto vp;
+    private VistaFactura vf;
     /**
      * Creates new form VentanaUsuario
      */
@@ -319,6 +320,16 @@ public class Principal extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+         jDesktopPane1.removeAll();
+            jDesktopPane1.repaint();
+            vf = new VistaFactura();
+            vf.toFront();
+            vf.setVisible(true);
+            jDesktopPane1.add(vf);
+            Dimension desktopSize = jDesktopPane1.getSize();
+            Dimension frameSize = vf.getSize();
+            vf.setLocation((desktopSize.width - frameSize.width) / 2, (desktopSize.height - frameSize.height) / 2);
+            vf.toFront();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
